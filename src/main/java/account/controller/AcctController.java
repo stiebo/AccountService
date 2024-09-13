48504b9a2184b495d4558f146c21564db056db04
stiebo <143817813+stiebo@users.noreply.github.com>
2024-09-13 12:06:09 +0200

    remove unnecessary ResponseEntity

--- src/main/java/account/controller/AcctController.java
+++ src/main/java/account/controller/AcctController.java
@@ -26,21 +26,17 @@ public class AcctController {
         this.mapper = payrollMapper;
     }
 
-    // should return statuscode 201
     @PostMapping("/payments")
-    // don't need to return response entity it is a rest controller
-    public ResponseEntity<Map<String, String>> uploadPayrolls(@Valid @RequestBody
-                                                                  List<UploadPayrollDto> uploadPayrollDtoList) {
-        // try to have all the mapping in the mapper
+    public Map<String, String> uploadPayrolls(@Valid @RequestBody List<UploadPayrollDto> uploadPayrollDtoList) {
         List<Payroll> payrollList = new ArrayList<>();
         uploadPayrollDtoList.forEach(uploadPayrollDto -> payrollList.add(mapper.toEntity(uploadPayrollDto)));
         service.savePayrolls(payrollList);
-        return ResponseEntity.ok(Collections.singletonMap("status", "Added successfully!"));
+        return Collections.singletonMap("status", "Added successfully!");
     }
 
     @PutMapping("/payments")
-    public ResponseEntity<Map<String, String>> updateSalary(@Valid @RequestBody UploadPayrollDto uploadPayrollDto) {
+    public Map<String, String> updateSalary(@Valid @RequestBody UploadPayrollDto uploadPayrollDto) {
         service.updateSalary(mapper.toEntity(uploadPayrollDto));
-        return ResponseEntity.ok(Collections.singletonMap("status", "Updated successfully!"));
+        return Collections.singletonMap("status", "Updated successfully!");
     }
 }
